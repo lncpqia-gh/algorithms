1f03ebc47dd4223704c9402ecde4e9aff70e8cd3
lncpqia_gh <lncpqia.lp@gmail.com>
2025-10-02 13:29:09 -0700

    Add initial files

---
+++ find_algorithm/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    // prompt: user for file name
+    // file check, prompt again if fail
+    // prompt: user for string to find (case sensitive)
+    //         user for new file to go thru
+    // show first find (string is in caps) with trailing characters on left and right
+    // prompt: user to choose new string
+    //         user to choose to go left
+    //         user to choose to go right
+}
